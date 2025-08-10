88e3e1d535a7821f15d7ba57bb6c19d64c7f930e
Om Official <omstha71@gmail.com>
2025-08-10 20:33:10 +0545

    functionaliyity almost completed, win and lost condition added and additional features added. design part left

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -1,11 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 const Hangman = () => {
+    const inputRef = useRef(null);
+
     const [comWord, setComWord] = useState("");
     const [inp, setInp] = useState('');
-    const [wrong, setWrong] = useState('');
-    const [count, setCount]= useState(0);
-    const [boxes, setBoxes] = useState([""])
+    const [wrong, setWrong] = useState([]);
+    const [count, setCount] = useState(0);
+    const [boxes, setBoxes] = useState([""]);
+    const [message, setMessage] = useState("");
+
 
     const words = ["apple", "ball", "cat", "danger", "eager", "gun"];
 
@@ -18,22 +22,50 @@ const Hangman = () => {
 
     const handelInp = (e) => {
         e.preventDefault();
+        if (count >= 6) return;
+        if (!boxes.includes("")) {
+            return;
+        }
+
         if (!inp) return;
+        if (wrong.includes(inp) || boxes.includes(inp)) {
+            setInp("");
+            setMessage("You already guessed :" + inp);
+            return;
+        }
 
         const newBoxes = [...boxes];
 
-        for (let i = 0; i < comWord.length ; i++) {
-            if(comWord[i] === inp){
+        for (let i = 0; i < comWord.length; i++) {
+            if (comWord[i] === inp) {
                 newBoxes[i] = inp;
             }
+        }
+
+        if (!comWord.includes(inp) && !wrong.includes(inp)) {
+            setCount((prev) => prev + 1);
+            setWrong((prev) => [...prev, inp])
         }
 
         setBoxes(newBoxes);
         setInp("");
     }
+
+    const handelReset = () => {
+        getWord();
+    }
+
     useEffect(() => {
         getWord();
     }, [])
+
+    useEffect(() => {
+        if (message) {
+            const timer = setTimeout(() => setMessage(""), 3000);
+            return () => clearTimeout(timer);
+        }
+    }, [message]);
+
     return (
         < div
             className="w-full p-4 md:p-20 flex flex-col justify-center items-center"
@@ -44,27 +76,52 @@ const Hangman = () => {
                 onSubmit={handelInp}
             >
                 <input
+                    ref={inputRef}
                     type="text"
                     value={inp}
                     maxLength={1}
                     placeholder='Character to check'
                     onChange={(e) => setInp((e.target.value).toUpperCase())}
+                    disabled={count >= 6 || !boxes.includes("")}
                     className='py-2 px-4 rounded border-2'
                 />
                 <button
                     type='submit'
-                    className='bg-green-400 hover:bg-green-500 py-2 px-4 rounded border-2'>
+                    disabled={count >= 6 || !boxes.includes("")}
+                    className='bg-green-400 hover:bg-green-500 py-2 px-4 rounded border-2'
+                >
                     Put
                 </button>
             </form>
             <ul className="flex gap-2">
                 {boxes.map((box, index) => (
-                    <li 
-                    className='w-10 h-10 border-2 flex justify-center items-center'
-                    key={index}>{box}</li>
+                    <li
+                        className='w-10 h-10 border-2 flex justify-center items-center'
+                        key={index}>{box}</li>
                 ))}
             </ul>
             <span>Commputer Generated : {comWord}</span>
+            <span
+                className='flex'
+            >
+                Wrong letter :
+                {wrong.map((wchar, index) => (
+                    <div
+                        className='mx-[4px] text-red-500'
+                        key={index}>{wchar}</div>
+                ))}
+            </span>
+            {!boxes.includes("") ? <div>You Won</div> : ""}
+            {count >= 6 ? <div> You Lost</div> : ""}
+
+            {message && <div className='text-red-500'>{message}</div>}
+            {(count >= 6 || !boxes.includes("")) &&
+                <button
+                    className='bg-green-400 hover:bg-green-500 py-2 px-4 rounded border-2'
+                    onClick={handelReset}
+                >
+                    Reset
+                </button>}
         </div>
     )
 }
